0596bb24d59f51cb86af03ed4e5b9860554ccee2
cyang <shmilycherish@gmail.com>
2019-07-12 18:41:13 +0800

    finish practice 4 in section 3

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,31 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  return minusCountVia(countCharacters(collectionA), objectB.value);
 }
+
+function countCharacters(collection) {
+  return collection.reduce((result, element) => {
+    let character = element.match(/[a-z]/g)[0];
+    let count = element.match(/\d+/g) != null ? parseInt(element.match(/\d+/g)[0]) : 1;
+    let object = result.filter(objectInResult => element.includes(objectInResult.key));
+    if (object.length !== 0) {
+      object[0].count += count;
+    } else {
+      result.push({key: character, count: count})
+    }
+    return result;
+  }, []);
+}
+
+function minusCountVia(collectionA, collectionB) {
+  return collectionA.map(object => {
+    collectionB.forEach(element => {
+      if (element === object.key) {
+        object.count -= Math.floor(object.count / 3);
+      }
+    });
+    return object;
+  });
+}
+
