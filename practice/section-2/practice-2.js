769fb2723f14e0616f7079c1a1cd6e617cf42574
cyang <shmilycherish@gmail.com>
2019-07-12 09:30:25 +0800

    finish practice 2 in section 2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let result = [];
+  countSameElements: for (let element of collection) {
+    let character = element.match(/[a-z]/g)[0];
+    let count = element.match(/\d+/g) != null ? parseInt(element.match(/\d+/g)[0]) : 1;
+    for (let object of result) {
+
+      if (element.includes(object.key)) {
+        object.count += count;
+        continue countSameElements;
+      }
+    }
+    result.push({key: character, count: count})
+  }
+  return result;
 }
