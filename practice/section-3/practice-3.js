d8a16b19b3ad8ddbedb0aae1e9c03eb568597314
cyang <shmilycherish@gmail.com>
2019-07-12 18:40:31 +0800

    finish practice 3 in section 3

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,30 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  return minusCountVia(countCharacters(collectionA), objectB.value)
+}
+
+function countCharacters(collectionA) {
+  let result = [];
+  countSameElements: for (let element of collectionA) {
+    for (let object of result) {
+      if (object !== undefined && object.key === element) {
+        object.count++;
+        continue countSameElements;
+      }
+    }
+    result.push({key: element, count: 1})
+  }
+  return result;
+}
+
+function minusCountVia(collectionA, collectionB) {
+  return collectionA.map(object => {
+    collectionB.forEach(element => {
+      if (element === object.key) {
+        object.count -= Math.floor(object.count / 3);
+      }
+    });
+    return object;
+  });
 }
